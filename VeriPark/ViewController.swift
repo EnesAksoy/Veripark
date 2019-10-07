ab6ccdeffbf2ce99222dc358903dfc6daa867230
EnesAksoy <enesaksoy1168@gmail.com>
2019-10-07 20:52:11 +0300

    Initial Commit

---
+++ VeriPark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VeriPark
+//
+//  Created by ENES AKSOY on 7.10.2019.
+//  Copyright © 2019 ENES AKSOY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
